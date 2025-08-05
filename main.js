612c753d7114c712cb05d41707f061faf7df8fd1
lipocodes <lipocodes@yahoo.com>
2025-08-05 11:32:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,35 +6,7 @@
     const resultBox = document.getElementById("result");
 
     /////////////////////////////////////////////////////////////////////////
-    const scrollContainer = document.querySelector('.table-scroll-container');   
-    let isDown = false;
-    let startX;
-    let scrollLeft;
 
-    scrollContainer.addEventListener('mousedown', (e) => {
-    isDown = true;
-    scrollContainer.classList.add('active');
-    startX = e.pageX - scrollContainer.offsetLeft;
-    scrollLeft = scrollContainer.scrollLeft;
-    });
-
-    scrollContainer.addEventListener('mouseleave', () => {
-    isDown = false;
-    scrollContainer.classList.remove('active');
-    });
-
-    scrollContainer.addEventListener('mouseup', () => {
-    isDown = false;
-    scrollContainer.classList.remove('active');
-    });
-
-    scrollContainer.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - scrollContainer.offsetLeft;
-    const walk = (x - startX) * 1.5; //scroll-fast
-    scrollContainer.scrollLeft = scrollLeft - walk;
-    });
 
 ////////////////////////////////////////////////////////////////////////
 
