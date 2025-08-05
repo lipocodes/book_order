34e70ff2189c7151d7a5ffb4d0efe9dc28936352
lipocodes <lipocodes@yahoo.com>
2025-08-05 11:21:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,39 @@
     const preview = document.getElementById("preview");
     const resultBox = document.getElementById("result");
 
+    /////////////////////////////////////////////////////////////////////////
+    const scrollContainer = document.querySelector('.table-scroll-container');   
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    scrollContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    scrollContainer.classList.add('active');
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    scrollContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+    });
+
+    scrollContainer.addEventListener('mouseup', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+    });
+
+    scrollContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 1.5; //scroll-fast
+    scrollContainer.scrollLeft = scrollLeft - walk;
+    });
+
+////////////////////////////////////////////////////////////////////////
+
     input.addEventListener("change", function () {
       const file = input.files[0];
       if (file) {
