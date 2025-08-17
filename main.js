ea8baaea334b6562b6a083911d11fc2b0fef50f6
lipocodes <lipocodes@yahoo.com>
2025-08-17 13:50:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,212 +1,148 @@
-let num_input = 0;
-let selectedFile1 = null;
-let selectedFile2 = null;
-let selectedFile3 = null;
-const container1 = document.getElementById('carouselContainer1'); 
-const container2 = document.getElementById('carouselContainer2');
-const container3 = document.getElementById('carouselContainer3'); 
-container1.style.display = "none";
-container2.style.display = "none";
-container3.style.display = "none"; 
-const input1 = document.getElementById("cameraInput1");
-const input2 = document.getElementById("cameraInput2");
-const input3 = document.getElementById("cameraInput3");
-//const preview = document.getElementById("preview");
-const resultBox1 = document.getElementById("result1");
-const resultBox2 = document.getElementById("result2");
-const resultBox3 = document.getElementById("result3");
-const fileName1 = document.getElementById("fileName1");
-const fileName2 = document.getElementById("fileName2");
-const fileName3 = document.getElementById("fileName3");
-const clear_carousel1 = document.getElementById("clear_carousel1");
-const clear_carousel2 = document.getElementById("clear_carousel2");
-const clear_carousel3 = document.getElementById("clear_carousel3");
-
-//////////////////////////////////////////////////
-input1.addEventListener("change", function () {
-const file = input1.files[0];
-if (file) {
- selectedFile1 = file;
- //preview.src = URL.createObjectURL(file);
- fileName1.textContent = input1.files[0].name;      
-   }
-});
-
-input2.addEventListener("change", function () {
-const file = input2.files[0];
-if (file) {
-selectedFile2 = file;
-//preview.src = URL.createObjectURL(file);
-fileName2.textContent = input2.files[0].name;      
-   }
-});
-
-input3.addEventListener("change", function () {
-const file = input3.files[0];
-if (file) {
-selectedFile3 = file;
-//preview.src = URL.createObjectURL(file);
-fileName3.textContent = input3.files[0].name;      
-   }
-});
-
-//////////////////////////////////////////////////////////////////
-function clearCarousel(num){ 
- if(num == 1){
-   container1.innerHTML = '';
-   clear_carousel1.style.display = "none";  
- }
- else if(num == 2){
-   container2.innerHTML = '';
-   clear_carousel2.style.display = "none";  
- }
- else if(num == 3){
-   container3.innerHTML = '';
-   clear_carousel3.style.display = "none";  
- }     
-      
-}
-
-//////////////////////////////////////////////////////////////
-function displayCarousel(books,num) {
-
-//before populating the carousel      
-if(num==1){
-  container1.style.display = "block";            
-  fileName1.textContent = '';      
-  container1.innerHTML = '';  //clear carousel content
-}
-else if(num==2){
-  container2.style.display = "block";            
-  fileName2.textContent = '';      
-  container2.innerHTML = ''; 
-}
-else if(num==3){
-  container.style.display = "block";            
-  fileName.textContent = '';      
-  container.innerHTML = '';
-}      
-
-
-books.forEach(book => {
-  const item = document.createElement('div');
-  item.className = 'carousel-item';
-
-  const dewey = document.createElement('div');
-  dewey.className = 'dewey';
-  dewey.textContent = book.dewey;
-
-  const title = document.createElement('div');
-  title.className = 'title';
-  title.textContent = book.title;
-
-  item.appendChild(dewey);
-  item.appendChild(title);
-   
-  if(num==1) {container1.appendChild(item); } 
-  else if(num==2) {container2.appendChild(item); }
-  else if(num==3) {container.appendChild(item);  }
- });
-}
-
-
-
-async function sendImage(num) {
-  //carousel needs to be empty
-  if(num==1) document.getElementById("carouselContainer1").innerHTML = "";
-  else if(num==2)  document.getElementById("carouselContainer2").innerHTML = "";
-  else if(num==3)  document.getElementById("carouselContainer").innerHTML = "";
-         
-  // taking a photo is compulsory..      
-  if (num==1 && !selectedFile1) {
-   alert("Please take a photo first.");
-   return;
-  }
-  else if (num==2 && !selectedFile2) {
-   alert("Please take a photo first.");
-   return;
-  }
-  else if (num==3 && !selectedFile3) {
-   alert("Please take a photo first.");
-   return;
-  }       
-      
-  // Update status
-  if(num==1) document.getElementById("status1").textContent = "⏳ Processing...";
-  else if(num==2) document.getElementById("status2").textContent = "⏳ Processing..."; 
-  else if(num==3) document.getElementById("status3").textContent = "⏳ Processing...";      
-      
-  try 
-  {
-   const formData = new FormData();
-   if(num==1) formData.append("image", selectedFile1);
-   else if(num==2) formData.append("image", selectedFile2);
-   else if(num==3) formData.append("image", selectedFile3);
-   
-   const response = await fetch("https://www.yvclib1.xyz/ocr/process", {
-      method: "POST",
-      body: formData
-    });
-   
-    const data = await response.json(); 
-    let list_books = [];
-    list_books.push(data);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="manifest" href="manifest.json" /> 
+  <title>Camera Upload</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="main.js" defer></script>
+</head>
+<body>
+  <div class="container">
+    <h4>Find Displaced Books</h4>
     
-   
-    //displayCarousel(list_books,num);  
-    const list_items = list_books[0]["sorted"];
-          
-    let books = [];
-    //If the response holds no items, the query has failed
-    if(list_items.length == 0 & num ==1){
-     document.getElementById("status1").textContent = "❌ The query has failed"; 
-     return;
-    }
-    else if(list_items.length == 0 & num ==2){
-     document.getElementById("status2").textContent = "❌ The query has failed";   
-     return;
-    }
-    else if(list_items.length == 0 & num ==3){
-     document.getElementById("status").textContent = "❌ The query has failed"; 
-     return;
-    }
-   
-    for(let i=0; i<list_items.length; i++){
-     const item = list_items[i];
-     const pos = item.indexOf("^^^");
-     const dewey = item.substr(pos+3);     
-     const title = item.substr(0,pos);
-      
-     let obj = {};
-     obj.dewey = dewey;
-     obj.title = title;         
-     books.push(obj);     
-    }
+    <div id="photo1">
+      <div style="font-size:14px;font-weight: bold;">📷 Photo 1:</div>
+      <label for="cameraInput1" class="custom-file-upload">📷 Take a Photo</label>
+      <input type="file" accept="image/*" id="cameraInput1" capture="environment" />
+      <button class="custom-file-upload" onclick="sendImage(1)">Send</button>
+      <span id="fileName1"></span>
+      <div id="result1">
+        <div id="status1"></div>
+        <div class="carousel-wrapper">
+          <div id="carouselContainer1" class="carousel-container"></div>
+            <button id="clear_carousel1" class="custom-file-upload" onclick="clearCarousel(1)">Clear</button> 
+          </div>
+      </div>  
+    </div>
+
+    <br/> 
     
-    displayCarousel(books,num);      
-          
-    if (!response.ok) {  
-      console.log("eeeeeeeeeeeeeeeeee=" + response.statusText);
-      throw new Error("Server error: " + response.statusText);
-    }
+    <div id="photo2">
+      <div style="font-size:14px;font-weight: bold;">📷 Photo 2:</div>
+      <label for="cameraInput2" class="custom-file-upload">📷 Take a Photo</label>
+      <input type="file" accept="image/*" id="cameraInput2" capture="environment" />
+      <button class="custom-file-upload" onclick="sendImage(2)">Send</button>
+      <span id="fileName2"></span>
+      <div id="result2">
+        <div id="status2"></div>
+        <div class="carousel-wrapper">
+          <div id="carouselContainer2" class="carousel-container"></div>
+            <button id="clear_carousel2" class="custom-file-upload" onclick="clearCarousel(2)">Clear</button> 
+          </div>
+      </div>  
+    </div>
 
-    //if the book check was not clean of errors      
-    if(num==1 && list_books[0]["existing_swaps"] == 1){
-     document.getElementById("status1").textContent = "❌ The right book order should be:";     
-    }else if(num==1){
-     document.getElementById("status1").textContent = "✅ No misplaced books have been found!";
-    }
-    else if(num==2 && list_books[0]["existing_swaps"] == 1){
-     document.getElementById("status2").textContent = "❌ The right book order should be:";     
-    }else if(num==2){
-     document.getElementById("status2").textContent = "✅ No misplaced books have been found!";
-    }  
-    else if(num==3 && list_books[0]["existing_swaps"] == 1){
-     document.getElementById("status").textContent = "❌ The right book order should be:";     
-    }else if(num==3){
-     document.getElementById("status").textContent = "✅ No misplaced books have been found!";
-    }       
+    <br/>
 
-  }
-  catch(error)  {  alert(error);  console.log("xxxxxxxxxxxx=" + error);  }        
-}
+    <div id="photo3">
+      <div style="font-size:14px;font-weight: bold;">📷 Photo 3:</div>
+      <label for="cameraInput3" class="custom-file-upload">📷 Take a Photo</label>
+      <input type="file" accept="image/*" id="cameraInput3" capture="environment" />
+      <button class="custom-file-upload" onclick="sendImage(3)">Send</button>
+      <span id="fileName3"></span>
+      <div id="result3">
+        <div id="status3"></div>
+        <div class="carousel-wrapper">
+          <div id="carouselContainer3" class="carousel-container"></div>
+            <button id="clear_carousel3" class="custom-file-upload" onclick="clearCarousel(3)">Clear</button> 
+          </div>
+      </div>  
+    </div>
+ 
+    
+
+  
+      
+  </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
