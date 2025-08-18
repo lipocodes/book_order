1efb4e8df78696cc78a07e448f90ee7371f32ab2
lipocodes <lipocodes@yahoo.com>
2025-08-18 14:12:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,11 @@ const lior3 = document.getElementById("lior3");
 //const carousel_items3 = document.getElementById("carousel_items3");
 //carousel_items1.style.display = "none";
 //carousel_items2.style.display = "none";
-//carousel_items3.style.display = "none"; 
+//carousel_items3.style.display = "none";
+
+lior1.style.display = "none";
+//carousel_items2.style.display = "none";
+//carousel_items3.style.display = "none";
 
 container1.style.display = "none";
 container2.style.display = "none";
