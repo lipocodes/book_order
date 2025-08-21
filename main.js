dc5fadfc7fc753aee7cfb7a97561b795e5202113
lipocodes <lipocodes@yahoo.com>
2025-08-21 09:49:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ try {
   cancelTimeout(timeoutId, () => {
     console.log("⏹ Timeout cleared successfully");
     alert("xxxxxxxxxxxxxxx=" + str(num));
-    /*if(num==1)*/ document.getElementById("status1").textContent = "❌ The query failed..";
+    if(num==1) document.getElementById("status1").textContent = "❌ The query failed..";
     /*else if(num==2)*/ document.getElementById("status2").textContent = "❌ The query failed..";
     /*else if(num==3)*/ document.getElementById("status3").textContent = "❌ The query failed..";
   });
