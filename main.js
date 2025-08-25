92545273dde3cca309b9d293bd98e6cdc3333efc
lipocodes <lipocodes@yahoo.com>
2025-08-25 10:19:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -122,6 +122,10 @@ else if(num==3){
 books.forEach(book => {
   const item = document.createElement('div');
   item.className = 'carousel-item';
+  item.style.backgroundColor = "#E6E6FA";
+  imgDiv.style.color = "white";
+  imgDiv.style.padding = "10px";
+  imgDiv.style.boxSizing = "border-box";
 
   const dewey = document.createElement('div');
   dewey.className = 'dewey';
