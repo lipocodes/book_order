4e42f765919721cef4aaa8ba164ffd9a7c3860c1
lipocodes <lipocodes@yahoo.com>
2025-09-15 15:37:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -290,7 +290,7 @@ async function sendImage(num) {
    else if(num==3) formData.append("image", selectedFile3);
 
    let data;
-   /*
+   
    //we need each <input> to have its separate fetch() operation
    if(num==1){
       const response1 = await fetch("https://www.yvclib.org/ocr/process", {
@@ -323,7 +323,7 @@ async function sendImage(num) {
     document.getElementById("status3").textContent = ""; 
     console.log("ccc=" + data.toString());    
    }
-       
+    /*   
     let list_books = [];
     list_books.push(data);
  
